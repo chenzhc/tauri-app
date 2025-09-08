067c4f643f8728aa846badbf461be4201c8d4438
chenzc <80132938@qq.com>
2025-09-08 14:07:43 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -16,21 +16,27 @@ async function greet() {
     <h1>Welcome to Tauri + Vue</h1>
 
     <div class="row">
-      <a href="https://vite.dev" target="_blank">
+      <a>
         <img src="/vite.svg" class="logo vite" alt="Vite logo" />
       </a>
-      <a href="https://tauri.app" target="_blank">
+      <a>
         <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
       </a>
-      <a href="https://vuejs.org/" target="_blank">
+      <a>
         <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
       </a>
     </div>
     <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
 
-    <form class="row" @submit.prevent="greet">
-      <input id="greet-input" v-model="name" placeholder="Enter a name..." />
-      <button type="submit">Greet</button>
+    <form class="row"  size="small">
+      <el-space wrap :size="10">
+        <el-input id="greet-input" 
+          v-model="name" 
+          style="width:200px;"
+          placeholder="Enter a name..." />
+        <el-button type="primary" size="small" @click="greet">Greet</el-button>
+      </el-space>
+      
     </form>
     <p>{{ greetMsg }}</p>
   </main>
@@ -102,20 +108,6 @@ h1 {
   text-align: center;
 }
 
-input,
-button {
-  border-radius: 8px;
-  border: 1px solid transparent;
-  padding: 0.6em 1.2em;
-  font-size: 1em;
-  font-weight: 500;
-  font-family: inherit;
-  color: #0f0f0f;
-  background-color: #ffffff;
-  transition: border-color 0.25s;
-  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.2);
-}
-
 button {
   cursor: pointer;
 }
@@ -131,10 +123,6 @@ button:active {
 input,
 button {
   outline: none;
-}
-
-#greet-input {
-  margin-right: 5px;
 }
 
 @media (prefers-color-scheme: dark) {
--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import locale from 'element-plus/es/locale/lang/zh-cn'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import Cookies from 'js-cookie'
 
 const app = createApp(App);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+// 使用element-plus 并且设置全局的大小
+app.use(ElementPlus, {
+  locale: locale,
+  // 支持 large、default、small
+  size: Cookies.get('size') || 'small' , 
+  zIndex: 3000 
+});
 
 app.mount("#app");
