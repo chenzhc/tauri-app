b3562f0e88d075ec57acac15b898ce7009ed14dc
chenzc <80132938@qq.com>
2025-09-09 17:55:47 +0800

    add

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,10 @@
 #![cfg_attr(debug_assertions, allow(dead_code, unused_imports, unused_variables, unused_mut))]
 #![allow(dead_code)]
 #![allow(unused_variables)]
+
+use std::sync::Mutex;
 use log::info;
+use tauri::{Listener, Manager};
 use tauri_plugin_fs::FsExt;
 
 pub mod mycustomcmd;
@@ -17,20 +20,37 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_serialplugin::init())
-        .plugin(tauri_plugin_fs::init())
+        .manage(mycustomcmd::Database{})
         .setup(|app| {
-          // 允许指定目录
-          let scope = app.fs_scope();
-          let _ = scope.allow_directory("C:\\opt\\upFiles\\temp", false);
+            app.manage(mycustomcmd::AppData {
+                welcome_message: "Welcome to Tauri!",
+            });
+            app.manage(Mutex::new(mycustomcmd::AppState::default()));
+            app.once("ready", |event| {
+                info!("app is ready!");
+            });
+            #[cfg(debug_assertions)] // only include this code on debug builds
+            {
+                // let window = app.get_window("main").unwrap();
+                // window.open_devtools();
+                // window.close_devtools();
+            }
           Ok(())
         })
+        .plugin(tauri_plugin_serialplugin::init())
+        .plugin(tauri_plugin_fs::init())        
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(
             tauri::generate_handler![
                 greet,
                 mycustomcmd::my_custom_command,
-                mycustomcmd::read_file
+                mycustomcmd::read_file,
+                mycustomcmd::login,
+                mycustomcmd::my_webview_label,
+                mycustomcmd::my_custom_command2,
+                mycustomcmd::hello,
+                mycustomcmd::get_app_data,
+                mycustomcmd::get_app_state
             ]
         )
         .run(tauri::generate_context!())
--- src-tauri/src/mycustomcmd.rs
+++ src-tauri/src/mycustomcmd.rs
@@ -2,8 +2,74 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use std::{io::Error, sync::Mutex};
 use log::info;
-use tauri::ipc::Response;
+use tauri::{ipc::Response, path::{BaseDirectory, PathResolver}, webview, Manager};
+
+pub struct Database;
+
+#[derive(serde::Serialize)]
+pub struct CustomResponse {
+    message: String,
+    other_val: usize,
+}
+
+pub struct AppData {
+    pub welcome_message: &'static str,
+}
+
+#[derive(Default)]
+pub struct AppState {
+    pub counter: u32,
+}
+
+pub async fn some_other_function() -> Option<String> {
+    Some("response".into())
+}
+
+#[tauri::command]
+pub fn get_app_data(app: tauri::AppHandle) -> String {
+    let data = app.state::<AppData>().welcome_message;
+    return data.to_string();
+}
+
+#[tauri::command]
+pub fn get_app_state(app: tauri::AppHandle) -> u32 {
+    let state = app.state::<Mutex<AppState>>();
+
+    let mut state = state.lock().unwrap();
+    state.counter += 1;
+
+    return state.counter;
+}
+
+#[tauri::command]
+pub fn hello(handle: tauri::AppHandle) -> String {
+    let resouce_path = handle.path()
+        .resolve("lang/de.json", BaseDirectory::Resource).unwrap();
+
+    let file = std::fs::File::open(&resouce_path).unwrap();
+    let lang_de: serde_json::Value = serde_json::from_reader(file).unwrap();
+
+    lang_de.get("hello").unwrap().to_string()
+}
+
+#[tauri::command]
+pub async fn my_custom_command2(
+    window: tauri::Window,
+    number: usize,
+    database: tauri::State<'_, Database>,
+) -> Result<CustomResponse, String> {
+    info!("Called from {}", window.label());
+    let result = some_other_function().await;
+    if let Some(message) = result {
+        Ok(
+            CustomResponse { message: message, other_val: 42 + number }
+        )
+    } else {
+        Err("No Result".into())
+    }
+}
 
 #[tauri::command(rename_all = "snake_case")]
 pub fn my_custom_command(invoke_message: String) -> String {
@@ -15,6 +81,22 @@ pub fn my_custom_command(invoke_message: String) -> String {
 pub fn read_file() -> Response {
     let data = std::fs::read("C:/opt/upFiles/temp").unwrap();
     return tauri::ipc::Response::new(data);
+}
+
+
+#[tauri::command]
+pub fn login(user: String, password: String) -> Result<String, String> {
+    if user == "tauri" && password == "tauri" {
+        Ok("logged_in".to_string())
+    } else {
+        Err("invalid credentials".to_string())
+    }
+}
+
+#[tauri::command]
+pub async fn my_webview_label(webview_window: tauri::WebviewWindow) {
+    info!("WebviewWindow: {}", webview_window.label());
+
 }
 
 #[cfg(test)]
